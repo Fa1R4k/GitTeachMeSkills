5fa9d7ebee2da92ae779a357cd6f5e0cba43f894
Max Zavalo <max.mine2104@gmail.com>
2022-11-02 21:38:00 +0300

    new commit notMain new wws

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello bug!");
     }
+
+    public static void merge(){
+        System.out.println("no merge!");
+    }
 }
